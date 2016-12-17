a0f91a8bbf08318f1a87c1d9eb590a78e111460d
chenrj23 <502111298@qq.com>
2016-12-17 19:39:53 +0800

    0.2

--- src/cTrip.js
+++ src/cTrip.js
@@ -2,18 +2,30 @@ const program = require('commander');
 const request = require('superagent');
 const charset = require('superagent-charset');
 const moment = require('moment');
+const CronJob = require('cron').CronJob;
 const log4js = require('log4js');
 const mysql = require('mysql');
 const connection = mysql.createConnection({
-    host: '127.0.0.1',
+    host: '120.27.5.155',
     user: 'root',
-    password: 'chenrj23',
+    password: '',
     database: 'cTrip',
 });
+
+const pool  = mysql.createPool({
+  connectionLimit : 10,
+  host            : '120.27.5.155',
+  user            : 'root',
+  password        : '',
+  database        : 'cTrip'
+});
+
+
 // extend with Request#proxy()
-require('superagent-proxy')(request);
+// require('superagent-proxy')(request);
 let proxy = 'http://218.106.205.145:8080';
-const collection = ['ZUHPVG', 'PVGZUH', 'KWLDLC', 'DLCKWL', 'PVGSYX', 'SYXPVG', 'PVGHRB', 'HRBPVG', 'CGOHRB', 'HRBCGO', 'PVGKWE', 'KWEPVG', 'ZUHCGO', 'CGOZUH', 'PVGKWL', 'KWLPVG']
+let catalogue = (new Date).getTime();
+const collection = ['PVGHRB', 'HRBPVG', 'PVGKWL', 'KWLPVG', 'SYXPVG', 'PVGSYX', 'PVGKWE', 'KWEPVG', 'KWEWNZ', 'WNZKWE', 'PVGZUH', 'ZUHPVG', 'ZUHCGO', 'CGOZUH', 'CGOHAK', 'HAKCGO', 'WNZHAK', 'HAKWNZ', 'ZUHTNA', 'TNAZUH', 'SZXTYN', 'TYNSZX', 'HETTYN', 'TYNHET', 'SZXHET', 'HETSZX']
 
 program
     .version('0.0.1')
@@ -22,18 +34,22 @@ program
     .option('-a, --arrAirportCode <code>', 'arrive airport code like BJS,PEK')
     .option('-l, --searchDayLong [number]', 'how many days search like 30')
     .option('-f, --searchDefault', 'searchDefault')
+    .option('-i, --insist [times]', 'search auto')
+    .option('-s, --speed [times]', 'search speed')
     // .option('-b, --debug [level]', '')
     .parse(process.argv);
 
-const deptAirportCode = program.deptAirportCode,
-    arrAirportCode = program.arrAirportCode,
+const deptAirportCode = program.deptAirportCode || false,
+    arrAirportCode = program.arrAirportCode || false,
     deptDate = program.deptDate || moment().format('YYYY-MM-DD'),
-    searchDayLong = program.searchDayLong || '1',
-    searchDefault = program.searchDefault || false;
+    searchDayLong = parseInt(program.searchDayLong) || 1,
+    searchDefault = program.searchDefault || false,
+    insist = program.insist || false,
+    speed = parseInt(program.speed) || 2000;
+
+
 
 const requsetAgain = 3;
-const catalogue = (new Date).getTime();
-let queryCount = searchDayLong;
 
 log4js.configure({
     appenders: [{
@@ -50,10 +66,9 @@ let loggerFile = log4js.getLogger('fileLog');
 
 // logger.debug(errHead)
 logger.setLevel('debug');
-logger.debug(queryCount)
-
-charset(request); // this will add request.Request.prototype.charset
+// this will add request.Request.prototype.charset
 // fix the superagent decode form gbk data
+charset(request);
 
 function connectMysql() {
 
@@ -77,7 +92,6 @@ function filter(resJson, deptDate, deptAirportCode, arrAirportCode) {
     if (resJson.Error) {
         loggerFile.error(deptAirportCode, arrAirportCode, deptDate, 'request error!')
         loggerFile.error(resJson.Error)
-
         return
     }
     let flightDataArrays = resJson.fis;
@@ -85,10 +99,10 @@ function filter(resJson, deptDate, deptAirportCode, arrAirportCode) {
     let filteredData = flightDataArrays.map(function(flightData) {
             let flightNo,
                 airlineCode,
-                depCity,
                 depAirport,
-                arrCity,
                 arrAirport,
+                depCity,
+                arrCity,
                 depDate,
                 depDateTime,
                 price,
@@ -103,10 +117,10 @@ function filter(resJson, deptDate, deptAirportCode, arrAirportCode) {
                 combinedTransport = 'none';
 
             flightNo = flightData.fn;
-            depCity = flightData.dcc;
             depAirport = flightData.dpc;
-            arrCity = flightData.acc;
             arrAirport = flightData.apc;
+            depCity = flightData.dcc;
+            arrCity = flightData.acc;
             fType = flightData.cf.c;
             depDateTime = flightData.dt;
             price = Number(flightData.lp);
@@ -161,12 +175,15 @@ function filter(resJson, deptDate, deptAirportCode, arrAirportCode) {
         })
         // logger.debug(filteredData)
 
-    connection.query('INSERT INTO flightsdata (airlineCode,flightNo,depDate,depDateTime,price,depCity,depAirport,arrCity,arrAirport,fType,isShare,shareFlight,isStopover,stopoverCity,isCombinedTransport,combinedTransport,catalogue) VALUES ?', [filteredData], function(err, result) {
+    pool.query('INSERT INTO flightsdata (airlineCode,flightNo,depDate,depDateTime,price,depCity,depAirport,arrCity,arrAirport,fType,isShare,shareFlight,isStopover,stopoverCity,isCombinedTransport,combinedTransport,catalogue) VALUES ?', [filteredData], function(err, result) {
         if (err) {
             // loggerFile.error(err)
             // loggerFile.error('INSERT INTO flightsdata (airlineCode,flightNo,depDate,depDateTime,price,depAirport,arrAirport,fType,isShare,shareFlight,isStopover,stopoverCity,isCombinedTransport,combinedTransport,catalogue) VALUES ?')
-            loggerFile.error([filteredData])
-            throw err;
+            loggerFile.error(deptDate, deptAirportCode, arrAirportCode, 'insert have an error')
+            loggerFile.error('flightDataArrays: ', flightDataArrays)
+            loggerFile.error('filteredData:', [filteredData])
+            loggerFile.error('insert error:', err)
+            return
         }
         logger.info(deptDate, deptAirportCode, arrAirportCode, 'insert')
             // queryCount--;
@@ -178,14 +195,13 @@ function filter(resJson, deptDate, deptAirportCode, arrAirportCode) {
     });
 }
 
-function reqCTrip(deptDate, deptAirportCode, arrAirportCode,errCount = requsetAgain) {
+function reqCTrip(deptDate, deptAirportCode, arrAirportCode, errCount = requsetAgain) {
     logger.info(deptDate, deptAirportCode, arrAirportCode,"resquest start")
     let errHead = `${deptDate} from ${deptAirportCode} to ${arrAirportCode} `
     let searchParam = setSearchParam(deptDate, deptAirportCode, arrAirportCode);
     // logger.info(errCount)
     request
         .get(searchParam)
-        // .proxy(proxy)
         .charset('gbk')
         .timeout(10000)
         .end(function(err, res) {
@@ -196,6 +212,7 @@ function reqCTrip(deptDate, deptAirportCode, arrAirportCode,errCount = requsetAg
                 loggerFile.error(errHead, `errCount: `, errCount)
                 if (errCount === 0) {
                     loggerFile.fatal(errHead, `request fail`)
+                    process.exit(1);
                 } else {
                     reqCTrip(deptDate, deptAirportCode, arrAirportCode, errCount)
                 }
@@ -210,34 +227,58 @@ function reqCTrip(deptDate, deptAirportCode, arrAirportCode,errCount = requsetAg
 
 
 function search(deptDate, deptAirportCode, arrAirportCode, searchDayLong) {
-    // const catalogue = (new Date).getTime()
-    if (searchDayLong === '1') {
+    if (searchDayLong === 1) {
         reqCTrip(deptDate, deptAirportCode, arrAirportCode);
     } else {
         let timeCount = 0;
         for (let i = 0; i < searchDayLong; i++) {
             let deptDateAdded = moment(deptDate).add(i, 'days').format('YYYY-MM-DD');
-            timeCount += 2000;
             setTimeout(function() {
-                reqCTrip(deptDateAdded, deptAirportCode, arrAirportCode);
+              reqCTrip(deptDateAdded, deptAirportCode, arrAirportCode);
             }, timeCount)
+            timeCount += speed;
+            // console.log(timeCount);
 
             // timeParams.push(timeParam)
         }
     }
 }
 
-if (searchDefault) {
-    let timeCount = 0
-    for (let route of collection) {
-        let deptAirportCode = route.slice(0, 3)
-        let arrAirportCode = route.slice(3, 6)
-        timeCount += 60000;
-        setTimeout(function() {
-            search(deptDate, deptAirportCode, arrAirportCode, searchDayLong)
-        }, timeCount)
-    }
+function longSearch(searchDayLong){
+  let timeCount = 0;
+  catalogue = (new Date).getTime();
+  loggerFile.debug('catalogue: ',catalogue)
+  for (let route of collection) {
+    let deptAirportCode = route.slice(0, 3)
+    let arrAirportCode = route.slice(3, 6)
+    // let deptDate = moment().format('YYYY-MM-DD')
+    setTimeout(function() {
+      search(deptDate, deptAirportCode, arrAirportCode, searchDayLong)
+    }, timeCount)
+    timeCount += searchDayLong*speed;
+  }
 }
 
-search(deptDate, deptAirportCode, arrAirportCode, searchDayLong);
+if (searchDefault) {
+  longSearch(searchDayLong)
+}
+
+
+if (deptDate && deptAirportCode && arrAirportCode && searchDayLong) {
+  search(deptDate, deptAirportCode, arrAirportCode, searchDayLong)
+}
+
+if (insist) {
+  var job = new CronJob({
+    cronTime: insist,
+    onTick: function() {
+      longSearch(searchDayLong)
+    },
+    start: false,
+    timeZone: 'Asia/Shanghai'
+  });
+  job.start();
+}
+
+// search(deptDate, deptAirportCode, arrAirportCode, searchDayLong);
 // setInterval(search, 10000)
