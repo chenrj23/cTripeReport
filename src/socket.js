33a60c4d4af123305f2a5f5ae54359614db8c78b
chenrj23 <502111298@qq.com>
2016-12-22 00:19:55 +0800

    learn socket

---
+++ src/socket.js
@@ -0,0 +1,20 @@
+const net = require('net');
+const server = net.createServer((c) => {
+  // 'connection' listener
+  console.log('client connected');
+  c.on('end', () => {
+    console.log('client disconnected');
+  });
+  c.on('data', (data) => {
+    console.log('client say');
+    console.log(data.toString());
+  });
+  c.write('hi\r\n');
+  c.pipe(c);
+});
+server.on('error', (err) => {
+  throw err;
+});
+server.listen(8124, () => {
+  console.log('server bound');
+});
